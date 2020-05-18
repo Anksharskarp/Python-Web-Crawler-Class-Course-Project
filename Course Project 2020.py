aaea8a17470193ccf9608dc99d3c05bb2bd83d4e
Anksharskarp <58315985+Anksharskarp@users.noreply.github.com>
2020-05-18 15:17:41 -0400

    Update Course Project 2020.py

--- Course Project 2020.py
+++ Course Project 2020.py
@@ -14,14 +14,16 @@ page = requests.get(url)
 soup = BeautifulSoup('page.content', 'html.parser')
 
 for data in soup:
-    event_title = soup.find_all('div', class_='amev-event-title')
-    event_time = soup.find_all('div', class_='amev-event-time headingtext')
-    event_desc = soup.find_all('div', class_='amev-event-description-full')
-    event_register = soup.find_all('button', class_='button eventRegButton')
-
+    event_title = soup.find_all('div', class_="amev-event-title")
     activities.append(event_title)
+
+    event_time = soup.find_all('div', class_="amev-event-time headingtext")
     time.append(event_time)
+
+    event_desc = soup.find_all('div', class_="amev-event-description-full")
     description.append(event_desc)
+
+    event_register = soup.find_all('button', class_="button eventRegButton registerForEvent")
     links.append(event_register)
 
 eventData = pd.DataFrame(
